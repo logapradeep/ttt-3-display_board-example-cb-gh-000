fb312c44d78a424fc9b31b425acef86bb9226a54
Loga Pradeep <logapradeep@gmail.com>
2017-09-27 09:25:48 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+a=["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 def display_board
-  a=["   ","   ","   ","   ","   ","   ","   ","   ","   "]
   puts "#{a(0)}|#{a(1)}|#{a(2)}"
   puts "#{a(3)}|#{a(4)}|#{a(5)}"
   puts "#{a(6)}|#{a(7)}|#{a(8)}"
